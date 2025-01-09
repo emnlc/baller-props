c528d3eccce3579f3fc653874d3f787a27a289f7
emmanuel cortes <emnlc.dev@gmail.com>
2025-01-08 17:13:18 -0700

    Landing page images added

--- client/src/pages/Landing/Feature.tsx
+++ client/src/pages/Landing/Feature.tsx
@@ -1,4 +1,7 @@
+import useWindowSize from "@/hooks/isMobile";
+
 function Feature() {
+  const isMobile = useWindowSize();
   return (
     <>
       <section className=" text-white py-16">
@@ -18,32 +21,34 @@ function Feature() {
                 also included.
               </p>
             </div>
-            <div className="p-8">
+            <div className="md:px-4 ">
               <img
-                src="/baller props 3.JPG"
+                src={`${
+                  isMobile
+                    ? "/LandingAssets/MobileView.png"
+                    : "/LandingAssets/DesktopView2.png"
+                }`}
                 alt="Current Season Player Stats"
-                className="w-full h-auto"
+                className="w-full"
               />
             </div>
           </div>
-          <div className="grid md:grid-cols-2 gap-8 items-center mb-16">
-            <div className="p-8">
-              <img
-                src="/baller props 3.JPG"
-                alt="Top 50 Player Props"
-                className="w-full h-auto"
-              />
+          <div className="grid md:grid-cols-2 gap-16 items-center mb-16">
+            <div className="flex flex-col justify-center text-center md:text-left">
+              <h2 className="text-3xl font-bold mb-4">Daily Props</h2>
+              <p className="text-lg text-gray-300">
+                Player props obtained through{" "}
+                <span className="text-[#8000FF]">PrizePicks</span> and update
+                regularly throught the day.
+              </p>
             </div>
             <div className="flex flex-col justify-center text-center md:text-left">
-              <h2 className="text-3xl font-bold mb-4">
-                Top 50 Player Props Spreadsheet
-              </h2>
+              <h2 className="text-3xl font-bold mb-4">Historical Data</h2>
               <p className="text-lg text-gray-300">
-                Spreadsheet
-                <span className="text-bpYellow"> available each day </span>
-                containing the
-                <span className="text-bpGreen"> top 50 player props</span> based
-                on their last 10 game stats.
+                Historical data from the{" "}
+                <span className="text-bpYellow">last two seasons</span> for{" "}
+                <span className="text-bpGreen">each individual player</span>{" "}
+                corresponding to the selected player prop.
               </p>
             </div>
           </div>
